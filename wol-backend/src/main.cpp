3950b0721919a1368e5cb581e31906322fef1dbf
Mandrusov Roman <mandrusov2005@mail.ru>
2025-04-09 18:22:06 +0300

    -

--- wol-backend/src/main.cpp
+++ wol-backend/src/main.cpp
@@ -1,9 +1,17 @@
+#include "ping.hpp"
+#include <asio/co_spawn.hpp>
+#include <asio/detached.hpp>
+#include <asio/io_context.hpp>
+#include <asio/use_awaitable.hpp>
 #include <core.hpp>
 
 int main() {
 
-    wol wol;
-    wol.run();
+    asio::io_context io;
+
+    core core(io, "30:9c:23:a6:80:5a", "192.168.1.131");
+    co_spawn(io, core.launching_device(), asio::detached);
+    core.run();
 
     return 0;
 }
