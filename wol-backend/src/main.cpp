84d75fcf634ca9c4b2df4c2b5b156990370bae84
Mandrusov Roman <mandrusov2005@mail.ru>
2025-04-09 19:54:07 +0300

    Change main func for params input

--- wol-backend/src/main.cpp
+++ wol-backend/src/main.cpp
@@ -5,11 +5,17 @@
 #include <asio/use_awaitable.hpp>
 #include <core.hpp>
 
-int main() {
+int main(int argc, char *argv[]) {
+
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <mac_address> <ip_address>\n";
+        return 1;
+    }
 
     asio::io_context io;
 
-    core core(io, "30:9c:23:a6:80:5a", "192.168.1.131");
+    core core(io, argv[argc - 2], argv[argc - 1]);
+
     co_spawn(io, core.launching_device(), asio::detached);
     core.run();
 
